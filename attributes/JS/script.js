bd5214c5af2e0a6502b390972aac8d9a0a3ff060
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-02-06 10:27:40 -0800

    YES

---
+++ attributes/JS/script.js
@@ -0,0 +1,24 @@
+let job = 
+document.getElementById("job")[0].id;
+
+function GAI(GAI) {
+    for(var i = 0; i < temp// length of arrays;
+    ; i++) {
+        if(job.islower = temp//array[i]
+        ) {
+            //array[i] = GAI
+            return GAI;
+        }
+    }
+    // return a popup saying Please check your spelling for the Job Input.
+}
+
+    GMI = GAI / 12; // calclating the Gross Monthly Income
+    HA = GMI * 0.33; // Housing Affordability
+    MC = GMI * 0.0145; //
+    PT = GMI * 0.12; //
+    ST = GMI * 0.07; //
+    SS = GMI * 0.82; //
+    SD = GMI * 0.01; //
+    RT = GMI * 0.05; //
+    MT = 180;
