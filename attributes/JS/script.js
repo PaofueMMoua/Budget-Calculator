6357193d1021f42c88776913fd18554e871c6946
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-02-06 13:01:21 -0800

    ...

--- attributes/JS/script.js
+++ attributes/JS/script.js
@@ -1,3 +1,5 @@
+import array from 
+// Career Stuff
 let job = 
 document.getElementById("job")[0].id;
 
@@ -15,10 +17,17 @@ function GAI(GAI) {
 
     GMI = GAI / 12; // calclating the Gross Monthly Income
     HA = GMI * 0.33; // Housing Affordability
-    MC = GMI * 0.0145; //
-    PT = GMI * 0.12; //
-    ST = GMI * 0.07; //
-    SS = GMI * 0.82; //
-    SD = GMI * 0.01; //
-    RT = GMI * 0.05; //
-    MT = 180;
+    MC = GMI * 0.0145; // Medicare 
+    FT = GMI * 0.12; // Federal Taxes
+    ST = GMI * 0.07; //State Taxes
+    SS = GMI * 0.82; // Sociall Security
+    SD = GMI * 0.01; // State Disability
+    RT = GMI * 0.05; // Retirement
+    MI = 180; // Medicare insurance
+
+
+// Return the Ammounts to the correct areas.
+
+
+// Checkbook
+
