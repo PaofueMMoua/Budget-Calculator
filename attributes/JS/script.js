7adbcbbe569bf82b3496f7dcbadca67630021e44
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-02-06 18:51:36 -0800

    GAI

--- attributes/JS/script.js
+++ attributes/JS/script.js
@@ -2,31 +2,29 @@ import data from EECU-data.js
 // Career Stuff
 let job = 
 document.getElementById("job")[0].id;
-
-function Gross(GAI) {
-    for(var i = 0; i < temp// length of arrays;
-    ; i++) {
-        if(job.isLower = temp//array[i]
-        ) {
-            let GAI = data.number[i];
-        }
+let j = 0
+for(var i = 0; i < temp// length of arrays;
+; i++) {
+    if(job.isLower = temp//array[i]
+    ) {
+        let GAI = data.number[i];
     }
-    // return a popup saying Please check your spelling for the Job Input.
+    j++;
 }
 
-    // let GMI = GAI / 12; // calclating the Gross Monthly Income
-    // let HA = GMI * 0.33; // Housing Affordability
-    // let MC = GMI * 0.0145; // Medicare 
-    // let FT = GMI * 0.12; // Federal Taxes
-    // let ST = GMI * 0.07; //State Taxes
-    // let SS = GMI * 0.82; // Sociall Security
-    // let SD = GMI * 0.01; // State Disability
-    // let RT = GMI * 0.05; // Retirement
-    // let MI = 180; // Medicare insurance
+    let GMI = GAI / 12; // calclating the Gross Monthly Income
+    let HA = GMI * 0.33; // Housing Affordability
+    let MC = GMI * 0.0145; // Medicare 
+    let FT = GMI * 0.12; // Federal Taxes
+    let ST = GMI * 0.07; //State Taxes
+    let SS = GMI * 0.82; // Sociall Security
+    let SD = GMI * 0.01; // State Disability
+    let RT = GMI * 0.05; // Retirement
+    let MI = 180; // Medicare insurance
 
 
 // Return the Ammounts to the correct areas.
-document.getElementById("").innerHTML = data.number[]
+document.getElementById("").innerHTML = data.number[i]
 document.getElementById("").innerHTML = GMI;
 document.getElementById("").innerHTML = GAI;
 document.getElementById("").innerHTML = HA;
