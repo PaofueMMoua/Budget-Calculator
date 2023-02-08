88617bedc688ed20adfaca9bab75c594a2e79b3d
PaofueMMoua <77351716+PaofueMMoua@users.noreply.github.com>
2023-02-07 23:44:41 -0800

    JS

--- attributes/JS/script.js
+++ attributes/JS/script.js
@@ -72,43 +72,89 @@ let data = [
 ]
 
 // Career Stuff
+// Job.addEventListener("change", (e) => FS());
+// function FS() {
+//     let jobI = Job.value;
+//     for(string of data){
+//         if(string[0]=jobI) {
+//             GAI=data[1];
+//             return GAI
+//         }
+//     }
+// }
+// let TEMP = document.getElementById("job");
+// function Checking() {
+//     let Job = document.getElementById('job').value;
+//     for (let i = 0; i < data.length; i++) {
+//         if (Job == data[i][0]) {
+//             let GAI = Number(data[i])[1];
+//             let GMI = GAI / 12; // calclating the Gross Monthly Income
+//             let HA = GMI * 0.33; // Housing Affordability
+//             let MC = GMI * 0.0145; // Medicare 
+//             let FT = GMI * 0.12; // Federal Taxes
+//             let ST = GMI * 0.07; //State Taxes
+//             let SS = GMI * 0.82; // Sociall Security
+//             let SD = GMI * 0.01; // State Disability
+//             let RT = GMI * 0.05; // Retirement
+//             let MI = 180; // Medicare insurance
+//             let TD = MC+FT+ST+SS+SD+RT+MI;
 
-let Job = document.getElementById("job");
-let GAI = 0;
-Job.addEventListener("change", (e) => FS());
-function FS() {
-    let jobI = Job.value;
-    for(string of data){
-        if(string[0]=jobI) {
-            GAI=data[1];
-            return GAI
+//             // Return the Ammounts to the correct areas.
+//             document.getElementById("GAI").innerHTML = GAI;
+//             document.getElementById("GMI").innerHTML = GMI;
+//             document.getElementById("GAI").innerHTML = GAI;
+//             document.getElementById("HA").innerHTML = HA;
+//             document.getElementById("M").innerHTML = MC;
+//             document.getElementById("FT").innerHTML = FT;
+//             document.getElementById("ST").innerHTML = ST;
+//             document.getElementById("SS").innerHTML = SS;
+//             document.getElementById("SD").innerHTML = SD;
+//             document.getElementById("RT").innerHTML = RT;
+//             document.getElementById("MI").innerHTML = MI;
+//             document.getElementById("TD").innerHTML = TD;
+
+//         }
+//     }
+// }
+let Job = document.getElementById('job');
+Job.addEventListener('change', checking);
+
+function checking() {
+    console.log(Job.value)
+    let Care = document.getElementById('job').value;
+        for (let i = 0; i < data.length; i++) {
+        if (Care == data[i][0]) {
+            let GAI = Number(data[i][1]);
+            let GMI = Math.round(GAI / 12); // calclating the Gross Monthly Income
+            let HA = Math.round(GMI * 0.33); // Housing Affordability
+            let MC = Math.round(GMI * 0.0145); // Medicare 
+            let FT = Math.round(GMI * 0.12); // Federal Taxes
+            let ST = Math.round(GMI * 0.07); //State Taxes
+            let SS = Math.round(GMI * 0.82); // Sociall Security
+            let SD = Math.round(GMI * 0.01); // State Disability
+            let RT = Math.round(GMI * 0.05); // Retirement
+            let MI = 180; // Medicare insurance
+            let TD = MC+FT+ST+SS+SD+RT+MI;
+
+            // Return the Ammounts to the correct areas.
+            document.getElementById("GAI").innerHTML = GAI;
+            document.getElementById('GAI-2').innerHTML = GAI;
+            document.getElementById("GMI").innerHTML = GMI;
+            document.getElementById('GMI-3').innerHTML = GMI;
+            // document.getElementById('GMI-4').innerHTML = GMI;
+            document.getElementById("HA").innerHTML = HA;
+            document.getElementById("M").innerHTML = MC;
+            document.getElementById("FT").innerHTML = FT;
+            document.getElementById("ST").innerHTML = ST;
+            document.getElementById("SS").innerHTML = SS;
+            document.getElementById("SD").innerHTML = SD;
+            document.getElementById("RT").innerHTML = RT;
+            document.getElementById("MI").innerHTML = MI;
+            document.getElementById("TD").innerHTML = TD;
+
         }
     }
-}
-// GAI = FS(Job)
+} 
 
-    let GMI = GAI / 12; // calclating the Gross Monthly Income
-    let HA = GMI * 0.33; // Housing Affordability
-    let MC = GMI * 0.0145; // Medicare 
-    let FT = GMI * 0.12; // Federal Taxes
-    let ST = GMI * 0.07; //State Taxes
-    let SS = GMI * 0.82; // Sociall Security
-    let SD = GMI * 0.01; // State Disability
-    let RT = GMI * 0.05; // Retirement
-    let MI = 180; // Medicare insurance
-    let TD = MC+FT+ST+SS+SD+RT+MI;
-
-// Return the Ammounts to the correct areas.
-document.getElementById("GAI").innerHTML =FS(Job);
-document.getElementById("GMI").innerHTML = GMI;
-document.getElementById("GAI").innerHTML = GAI;
-document.getElementById("HA").innerHTML = HA;
-document.getElementById("M").innerHTML = MC;
-document.getElementById("FT").innerHTML = FT;
-document.getElementById("ST").innerHTML = ST;
-document.getElementById("SS").innerHTML = SS;
-document.getElementById("SD").innerHTML = SD;
-document.getElementById("RT").innerHTML = RT;
-document.getElementById("MI").innerHTML = MI;
-document.getElementById("TD").innerHTML = TD;
+// Checking(TEMP)
 // Checkbook
